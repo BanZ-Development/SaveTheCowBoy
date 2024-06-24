57163a8ea4827e24ee6509a153371a475322681b
jackgimre <66290453+jackgimre@users.noreply.github.com>
2024-06-24 14:25:16 -0500

    Like bug fix and profile start

---
+++ public/js/profile.js
@@ -0,0 +1,34 @@
+function loadProfile() {
+	const uid = returnID();
+	console.log(uid);
+
+	const data = new FormData();
+	data.append('uid', uid);
+	fetch('api/auth/profile', {
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: new URLSearchParams(data)
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			createProfile(data.profile);
+		});
+}
+
+function returnID() {
+	let urlParams = window.location.search;
+	let getQuery = urlParams.split('?')[1];
+	if (!getQuery) return null;
+	let params = getQuery.split('&');
+	let id = params[0].split('=')[1];
+	return id;
+}
+
+function createProfile(profile) {
+	console.log(profile.username);
+}
+
+loadProfile();
