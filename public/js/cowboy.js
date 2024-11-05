045b8e6e808a173fda7e2ff15b13c4649edf12ba
Lucky <68414136+Raidlucky@users.noreply.github.com>
2024-11-05 15:48:54 -0700

    Update cowboy.js

--- public/js/cowboy.js
+++ public/js/cowboy.js
@@ -1,28 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('#storyInput').addEventListener('click', function () {
-		let page = document.querySelector('.createStory');
-		page.style = 'height: 80%; flex-direction: column; width: 95%;';
-		document.querySelector('#createStoryTitle').style = 'display: block; padding: 10px; font-family: "Roboto"; background-color: #fff;';
-		document.querySelector('#storyInput').style = 'margin-inline: auto; width: 70%; margin-top: 10px; border: solid 1px #ccc; padding: 10px;';
-		document.querySelector('#postStoryBtn').addEventListener('click', postStory);
-	});
+    document.querySelector('#storyInput').addEventListener('click', function () {
+            let page = document.querySelector('.createStory');
+            page.style = 'height: 60vh; flex-direction: column; width: 95%;';
+            document.querySelector('#createStoryTitle').style = 'display: block; padding: 10px; font-family: "Roboto"; background-color: #fff;';
+            document.querySelector('#createStoryDesc').style = 'display: block; padding: 10px; font-family: "Roboto"; background-color: #fff;';
+            document.querySelector('#storyInput').style = 'margin-inline: auto; width: 70%; margin-top: 10px; border: solid 1px #ccc; padding: 10px;';
+    });
 });
 
 document.addEventListener('click', function (event) {
 	let shrinkableDiv = document.querySelector('.createStory');
 	if (!shrinkableDiv.contains(event.target)) {
-		hideCreateStoryMenu();
+		shrinkableDiv.style = 'flex-direction: row; width: 50%;';
+		document.querySelector('#createStoryTitle').style = 'display: none;';
+		document.querySelector('#storyInput').style = 'margin-inline: none; width: calc(100% - 125px); margin-top: 0px; border: none; padding: 0px;';
 	}
 });
 
 loadStories();
 
-function hideCreateStoryMenu() {
-	let shrinkableDiv = document.querySelector('.createStory');
-	shrinkableDiv.style = 'flex-direction: row; width: 50%;';
-	document.querySelector('#createStoryTitle').style = 'display: none;';
-	document.querySelector('#storyInput').style = 'margin-inline: none; width: calc(100% - 125px); margin-top: 0px; border: none; padding: 0px;';
-}
 
 const InputValidation = (element, color) => {
 	const e = document.querySelector('#' + element);
